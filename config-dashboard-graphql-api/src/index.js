e2ece2744c59dd0cb827a97e9ef59c24053d88f5
Lanil Marasinghe <lanil@bitbucket.org>
2021-09-28 23:56:19 +0530

    prisma setup

--- config-dashboard-graphql-api/src/index.js
+++ config-dashboard-graphql-api/src/index.js
@@ -1,27 +1,53 @@
-const { ApolloServer } = require('apollo-server');
+const {ApolloServer} = require('apollo-server');
+const fs = require('fs');
+const path = require('path')
+const { PrismaClient } = require('@prisma/client')
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+const prisma = new PrismaClient()
 
-// 2
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+ },
+  // 3
+
+  Mutation: {
+    // 2
+    post: (parent, args, context, info) => {
+      return context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+    },
+  },
+
 }
 
 // 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+      path.join(__dirname, 'schema.graphql'),
+      'utf8'
+  ),
   resolvers,
+  context: {
+    prisma,
+  }
 })
 
 server
     .listen()
-    .then(({ url }) =>
+    .then(({url}) =>
         console.log(`Server is running on ${url}`)
     );
