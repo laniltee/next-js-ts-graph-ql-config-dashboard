b0c7fd222f7d50f83fe50d850e9f7d99939665b4
Lanil Marasinghe <lanil@bitbucket.org>
2021-09-30 19:54:07 +0530

    added antd form to addNewConfiguration.tsx

--- config-dashboard-ui/pages/configurations/addNewConfiguration.tsx
+++ config-dashboard-ui/pages/configurations/addNewConfiguration.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Form, Input, Typography, Select } from "antd";
+import { Button, Card, Form, Input, Typography, Select, Space } from "antd";
 import { useQuery, gql } from "@apollo/client";
 import { Tag } from "../../types/graphQlTypes";
 
@@ -14,20 +14,47 @@ const TAGS_QUERY = gql`
   }
 `;
 
+type ConfigurationInputs = {
+  name: string;
+  description: string;
+};
+
 const AddNewConfiguration = (): JSX.Element => {
   const { data } = useQuery(TAGS_QUERY);
+  const [form] = Form.useForm();
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFinish = (values: ConfigurationInputs) => {
+    console.log(values);
+  };
 
   return (
     <Card>
       <Title level={5}>Add New </Title>
-      <Form layout="vertical">
-        <Form.Item label="Name">
-          <Input placeholder="input placeholder" />
+      <Form
+        layout="vertical"
+        form={form}
+        name="control-hooks"
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[{ required: true, message: "Please input Name!" }]}
+        >
+          <Input placeholder="Name" />
         </Form.Item>
-        <Form.Item label="Description">
-          <Input placeholder="input placeholder" />
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[{ required: true, message: "Please input Description!" }]}
+        >
+          <Input placeholder="Description" />
         </Form.Item>
-        <Form.Item label="Tags">
+        <Form.Item name="tags" label="Tags">
           <Select
             mode="tags"
             style={{ width: "100%" }}
@@ -40,10 +67,15 @@ const AddNewConfiguration = (): JSX.Element => {
             ))}
           </Select>
         </Form.Item>
-        <Form.Item>
-          <Button type="primary" block>
-            Add{" "}
-          </Button>
+        <Form.Item className="text-right">
+          <Space>
+            <Button type="default" htmlType="button" onClick={onReset}>
+              Reset{" "}
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Add{" "}
+            </Button>
+          </Space>
         </Form.Item>
       </Form>
     </Card>
